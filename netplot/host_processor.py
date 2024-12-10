d45ed2b31e749e98a21d2730b8a2b6d64c006270
Albert <poisk123@yandex.ru>
2024-12-10 12:21:37 +0300

    Add files via upload

---
+++ netplot/host_processor.py
@@ -0,0 +1,20 @@
+from scapy.all import IP
+import socket
+
+from processor.processor import Processor
+
+
+class HostProcessor(Processor):
+    def __init__(self, config, domain_provider):
+        self.config = config
+        self.data = []
+        self.domain_provider = domain_provider
+
+    def process(self, packet):
+        if self.config.incoming:
+            ip = packet[IP].src
+        else:
+            ip = packet[IP].dst
+        self.data.append(self.domain_provider.get_domain(ip))
+
+
---
+++ netplot/processor.py
@@ -0,0 +1,3 @@
+class Processor:
+    def process(self, packet):
+        raise Exception("Not implemented!")
